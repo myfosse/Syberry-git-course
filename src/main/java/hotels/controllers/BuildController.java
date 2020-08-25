b03f987cc70fd5563c381dda3c29deea0aa7185b
Maria Peredry <m.peredry@syberry.com>
2020-08-25 23:45:54 +0300

    Revert / route

---
+++ src/main/java/hotels/controllers/BuildController.java
@@ -0,0 +1,14 @@
+package hotels.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RestController
+public class BuildController {
+    @GetMapping(path = "/")
+    @ResponseBody
+    public String getBuild() {
+        return "Build 0.0.1";
+    }
+}
