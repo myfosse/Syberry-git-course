487693070b6968c04e0123cb8efcef6473ca56cf
Alexander Khalimantsevich <a.khalimantsevich@syberry.com>
2020-08-26 19:09:38 +0300

    Fix cors

--- src/main/java/hotels/RunApplication.java
+++ src/main/java/hotels/RunApplication.java
@@ -2,11 +2,24 @@ package hotels;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RunApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 }
